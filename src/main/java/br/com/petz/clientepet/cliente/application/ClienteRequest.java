9f27b49ff8dda5e65f58fb87921744bf4ab25990
Ranylson Santos <ranylson1212@gmail.com>
2023-11-22 19:09:13 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/ClienteRequest.java
@@ -1,4 +1,28 @@
 package br.com.petz.clientepet.cliente.application;
 
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
 }
