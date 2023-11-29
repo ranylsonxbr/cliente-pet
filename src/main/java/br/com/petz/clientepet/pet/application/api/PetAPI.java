560cd5b70ae1cba4a604afaeafd4d7099f298a67
Ranylson Santos <ranylson1212@gmail.com>
2023-11-29 19:06:18 -0300

    implementa request e response

---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -0,0 +1,18 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/cliente/{idCliente}/pet")
+public interface PetAPI {
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    PetResponse postPet(@PathVariable UUID idCliente,
+                        @Valid @RequestBody PetRequest petRequest);
+
+}
---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
@@ -0,0 +1,33 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import lombok.Value;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Value
+public class PetRequest {
+    @NotBlank
+    private String nomePet;
+    private Porte porte;
+    @NotNull
+    private TipoPet tipoPet;
+    private String microchip;
+    @NotBlank
+    private String raca;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private SexoPet sexoPet;
+    private String pelagemCor;
+    @NotNull
+    private LocalDate dataNascimento;
+    private String rga;
+    private Integer peso;
+
+}
