8138eb0a149086914204787796f0c6c8bc6ce0e0
Ranylson Santos <ranylson1212@gmail.com>
2023-11-29 19:26:15 -0300

    implementa fluxo service, teste ok

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -1,19 +1,25 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import br.com.petz.clientepet.pet.application.service.PetService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PetController implements PetAPI {
+    private final PetService petService;
 
     @Override
-    public PetResponse postPet(UUID idCliente, PetRequest petRequest) {
+    public PetResponse postPet(UUID idCliente,@Valid PetRequest petRequest) {
         log.info("[inicia] PetController - postPet");
         log.info("[idCliente] {}",idCliente);
+        PetResponse pet = petService.criaPet(idCliente,petRequest);
         log.info("[finaliza] PetController - postPet");
-        return null;
+        return pet;
     }
 }
