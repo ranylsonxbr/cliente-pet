d2a04b5d8dffbdb9b5172c70303bfd8fe2af6843
Ranylson Santos <ranylson1212@gmail.com>
2023-12-01 21:07:06 -0300

    implementa fluxo pet service, busca pets do cliente

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
@@ -1,6 +1,7 @@
 package br.com.petz.clientepet.pet.application.service;
 
 import br.com.petz.clientepet.cliente.service.ClienteService;
+import br.com.petz.clientepet.pet.application.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.application.api.PetRequest;
 import br.com.petz.clientepet.pet.application.api.PetResponse;
 import br.com.petz.clientepet.pet.domain.Pet;
@@ -9,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,5 +28,14 @@ public class PetApplicationService implements PetService {
         Pet pet = petRepository.salvaPet(new Pet(idCliente,petRequest));
         log.info("[finaliza] PetApplicationService - criaPet");
         return new PetResponse(pet.getIdPet());
+    }
+
+    @Override
+    public List<PetClienteListResponse> buscaPetsDoClienteComId(UUID idCliente) {
+        log.info("[inicia] PetApplicationService - buscaPetsDoClienteComId");
+        clienteService.buscaClienteAtravesId(idCliente);
+        log.info("[finaliza] PetApplicationService - buscaPetsDoClienteComId");
+
+        return null;
     }
 }
